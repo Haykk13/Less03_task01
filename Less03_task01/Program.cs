d92a13908b2850a2992313d2a823250774aede95
Hayk <hayk.hayr3@gmail.com>
2020-12-05 21:10:53 +0400

    Add project files.

---
+++ Less03_task01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Less03_task01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Derived inst = new Derived();
+			Console.WriteLine("f1 = {0}\tf2 = {1}", inst.field1, inst.field2);
+
+			Console.ReadKey();
+		}
+	}
+
+	class Base
+	{
+		public string field1 = "base.f1";
+		public string field2 = "base.f2";
+	}
+	class Derived : Base
+    {
+		public string field1 = "derived.f1";
+
+	}
+}
